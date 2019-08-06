80b37c219f456c21299ed95c0f4386824c11df9b
Megan White <meganwhite@Megans-MacBook-Pro.local>
2019-08-06 15:12:18 -0700

    rendered user profile to screen

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  getUsers = () => {
+    axios
+      .get('https://api.github.com/users/meganwhite')
+      .then(response => this.setState({ users: response.data }))
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Github Users</h1>
+        <p>{this.state.users.name}</p>
+      </div>
+    );
+  }
 }
 
-export default App; 
+export default App;
