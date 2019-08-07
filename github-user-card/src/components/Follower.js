9d3461543a71b375fcd5453666b260b5fcd3fd59
Megan White <meganwhite@Megans-MacBook-Pro.local>
2019-08-07 15:07:46 -0700

    updated styling

--- github-user-card/src/components/Follower.js
+++ github-user-card/src/components/Follower.js
@@ -4,9 +4,15 @@ import { Card } from 'semantic-ui-react'
 const Follower = props => {
     return (
         <Card >
-            <div className='ui card'>
-                <img src = {props.follower.avatar_url} alt = "user avatar"/>
-                <h2>{props.follower.login}</h2>
+            <div className='ui cards'>
+                <div class="card">
+                    <div class="image">
+                        <img src = {props.follower.avatar_url} alt = "user avatar"/>
+                    </div>
+                    <div class="content">
+                        <h2>{props.follower.login}</h2>
+                    </div>
+                </div>
             </div>
         </Card>
 
